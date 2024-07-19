ad6a32e4727803108a7a378cf3ef3783c3332a8d
Mathis <mathisfrancois9@gmail.com>
2024-07-19 21:53:00 +0200

    Add comment

--- Usermode/PEinspector/main.cpp
+++ Usermode/PEinspector/main.cpp
@@ -1,6 +1,12 @@
 #include "PEParser.h"
 #include <iostream>
 
+/*
+	Je réfléchis encore à la structure du projet.
+	Donc si vous voyez une Class avec des statics alors qu'elles doivent être private c'est normal mdr.
+	Le code est loin d'être fini :)
+*/
+
 int main() {
 	PEParser peParser("C:\\Program Files\\Recuva\\recuva64.exe");
 	if (peParser.b_error) {
